419123658b9896b6e5b7b9797de8e707634bf235
wangzhipeng <1226294951@qq.com>
2020-11-13 19:45:16 +0800

    http & grpc 的header 自定义

--- app/server_grpc/main.go
+++ app/server_grpc/main.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
 	"github.com/zhipenwang/grpc_gateway/internal/conf"
 	rpc_proto "github.com/zhipenwang/grpc_gateway/internal/grpcfile"
+	"google.golang.org/grpc/metadata"
 	"log"
 	"net"
 	"google.golang.org/grpc"
@@ -14,7 +17,16 @@ type HelloServer struct {
 }
 
 func (h *HelloServer) SayHello(ctx context.Context, request *rpc_proto.Request) (*rpc_proto.Response, error) {
+	md, ok := metadata.FromIncomingContext(ctx)
+	if ok {
+		md_json, _ := json.Marshal(md)
+		log.Printf("header: %v", md)
+		fmt.Println(string(md_json))
+	}
 	log.Printf("receive msg: %v", request)
+	// set grpc response header
+	mds := metadata.Pairs("response-code", "123")
+	grpc.SetHeader(ctx, mds)
 	return &rpc_proto.Response{
 		Message: "hello " + request.Name,
 	}, nil
--- app/server_http/main.go
+++ app/server_http/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"github.com/grpc-ecosystem/grpc-gateway/runtime"
 	"github.com/zhipenwang/grpc_gateway/internal/conf"
 	rpc_proto "github.com/zhipenwang/grpc_gateway/internal/grpcfile"
@@ -13,11 +14,61 @@ import (
 func main() {
 	ctx := context.Background()
 	dopts := []grpc.DialOption{grpc.WithInsecure()}
-	gwMux := runtime.NewServeMux()
+	gwMux := runtime.NewServeMux(
+		runtime.WithIncomingHeaderMatcher(CustomIncomingHeader),
+		runtime.WithOutgoingHeaderMatcher(CustomOutcomingHeader),
+	)
 	if err := rpc_proto.RegisterHelloHandlerFromEndpoint(ctx, gwMux, conf.ServerAddr, dopts); err != nil {
 		log.Fatalf("failed to register gw server, err=%v", err)
 	}
 	if err := http.ListenAndServe(conf.ServerHttpAddr, gwMux); err != nil {
 		log.Fatalf("failed http server, err=%v", err)
+	}
+}
+
+/**
+自定义请求头的key，将http中的header—key转换为自定义的grpc-header-key，同时保留其他的映射规则
+default中会把 runtime-context.go中isPermanentHTTPHeader，不符合的key去掉
+如果是直接返回key的话，就不会被过滤，直接传输到grpc-header了
+
+### 注意：key需要首字母大写；http层的header规则
+
+example：
+下面替换后，到grpc-header的结果是：
+Origin-Key	=> origin-key
+Custom-Key	=> grpc-metadata-custom-key
+其他未指明替换的会走default进行替换，如：test-header-key，由于过滤，在grpc-header中就不会存在
+ */
+
+func CustomIncomingHeader(header_key string) (string, bool) {
+	switch header_key {
+	case "Origin-Key":
+		return header_key, true
+	case "Custom-Key":
+		return "Grpc-Metadata-" + header_key, true
+	default:
+		return runtime.DefaultHeaderMatcher(header_key)
+	}
+}
+
+/**
+自定义响应头的key，将grpc中的header—key转换为自定义的http-header-key，同时保留其他的映射规则
+default中会把 runtime-context.go中isPermanentHTTPHeader，不符合的key去掉
+如果是直接返回key的话，就不会被过滤，直接传输到http-header了
+
+### 注意：key按照grpc返回的响应header-key书写即可，http收到的header-key会是首字母大写的（http层的header规则）
+
+example：
+下面替换后，到http-header的结果是：
+response-code	=> Response-Code
+其他未指明替换的会走default进行替换，如：test-header-key，由于过滤，在http-header中就不会存在
+*/
+func CustomOutcomingHeader(header_key string) (string, bool) {
+	fmt.Println(header_key)
+	switch header_key {
+	case "response-code":
+		return header_key, true
+	default:
+		return runtime.DefaultHeaderMatcher(header_key)
 	}
 }
